d056ac1e6f700a0b192fe2b26b5b0e1525440573
Yogesh Mishra <yogeshmishra1007@gmail.com>
2026-02-02 11:27:33 +0530

    Initial project structure: Added Models and TaskLogic

---
+++ ProTrack/Logic/TaskLogic.cs
@@ -0,0 +1,33 @@
+﻿using ProTrack.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProTrack.Logic
+{
+    public class TaskLogic
+    {
+        // A private list to store our tasks in memory while the app is running
+        private readonly List<TaskItem> _tasks = new List<TaskItem>();
+        private int _nextId = 1;
+
+        // Method to add a new task
+        public void AddTask(string title, string? description)
+        {
+            var newTask = new TaskItem
+            {
+                Id = _nextId++,
+                Title = title,
+                Description = description,
+                Status = TaskStatus.Todo
+            };
+            _tasks.Add(newTask);
+        }
+
+        // Method to get all tasks
+        public List<TaskItem> GetAllTasks()
+        {
+            return _tasks;
+        }
+    }
+}
---
+++ ProTrack/Models/TaskItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProTrack.Models
+{
+    public enum TaskStatus
+    {
+        Todo,
+        InProgress,
+        Completed
+    }
+
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; } // 'required' ensures a task always has a name
+        public string? Description { get; set; }   // '?' means the description can be empty (null)
+        public TaskStatus Status { get; set; } = TaskStatus.Todo;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
