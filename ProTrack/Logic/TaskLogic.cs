05750d7166fc7016f8071cef303f87dddbc677ab
Yogesh Mishra <yogeshmishra1007@gmail.com>
2026-02-03 13:40:31 +0530

    added comments to understand whats going on

--- ProTrack/Logic/TaskLogic.cs
+++ ProTrack/Logic/TaskLogic.cs
@@ -9,6 +9,9 @@ namespace ProTrack.Logic
     {
         // A private list to store our tasks in memory while the app is running
         private readonly List<TaskItem> _tasks = new List<TaskItem>();
+        //List is a dynamic array here from System.Collections.Generic
+        //TaskItem yaha pe is dataType(Generic/Template) that u can only store TaskItem ke objects here
+        //_tasks is reference type variable it stores the address of the list which is on heap memory
         private int _nextId = 1;
 
         // To add a new task
@@ -16,6 +19,7 @@ namespace ProTrack.Logic
         {
             var newTask = new TaskItem
             {
+                //this is Object initializer...one way to create objext and initialize without calling contructor.
                 Id = _nextId++,
                 Title = title,
                 Description = description,
